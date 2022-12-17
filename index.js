7bb6048bedd45acafec5e54baaf1a6d0d6651994
Aura <ax.pink@gmail.com>
2022-12-17 14:35:46 -0500

    commit tree b8561b1c908f747e4f212df729e19a99cd63614f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+    const name = ["Guadalupe", "Ollie", "Aki"]
+
+    function writeCards(name, event) {
+        const thanksMsg = []
+        for(let i = 0; i < name.length; i++) {
+            thanksMsg.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        }
+        return thanksMsg  
+      }
+    
+      function countDown(num) {
+        while (num >= 0) {
+        console.log(num--
+        )
+        }
+      }
